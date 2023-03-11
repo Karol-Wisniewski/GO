1ad6ea5afb06edc6bf6342ffbc98ef60f83ebafb
Karol Wiśniewski <k.wisniewski.417@studms.ug.edu.pl>
2023-03-11 17:59:05 +0100

    Done first task

--- Collatz/Collatz.go
+++ Collatz/Collatz.go
@@ -1,51 +1,86 @@
 package main
 
 import (
-	"flag"
 	"fmt"
-	"math"
-	"strconv"
 )
 
-func even(n float64) (b bool) {
-	if math.Mod(n, 2) == 0 {
+func even(n int) (b bool) {
+	if n%2 == 0 {
 		b = true
 	} else {
 		b = false
 	}
-	return
+	return b
 }
 
-func odd(n float64) (b bool) {
+func odd(n int) (b bool) {
 	if even(n) {
 		b = false
 	} else {
 		b = true
 	}
-	return
+	return b
 }
 
-func collatz(n float64) {
+func collatz(n int, temp []int) []int {
 	switch {
 	case n == 1:
-		fmt.Println("done!")
+		break
 
 	case even(n):
 		n = n / 2
-		fmt.Println(n)
-		collatz(n)
+		temp = append(temp, n)
+		return collatz(n, temp)
 
 	case odd(n):
-		n = n*3 + 1
-		fmt.Println(n)
-		collatz(n)
+		n = (n * 3) + 1
+		temp = append(temp, n)
+		return collatz(n, temp)
 	}
+	return temp
 }
 
 func main() {
-	flag.Parse()
+	//slice to hold the Collatz sequence for each number in range
+	temp := []int{}
 
-	n, _ := strconv.ParseFloat(flag.Arg(0), 64)
+	//length of the longest Collatz sequence in provided range
+	var longest int
 
-	collatz(n)
+	//number that produces the longest Collatz sequence in provided range
+	var number int
+
+	//lower bound of range to check
+	var l int
+
+	//upper bound of range to check
+	var h int
+
+	fmt.Println("Enter the first number of range to check: ")
+	_, errl := fmt.Scan(&l)
+	if errl != nil {
+		fmt.Println(errl)
+		return
+	}
+
+	fmt.Println("Enter the last number of range to check: ")
+	_, errh := fmt.Scan(&h)
+	if errh != nil {
+		fmt.Println(errh)
+		return
+	}
+
+	for i := l; i < h+1; i++ {
+		temp = collatz(i, temp)
+		if len(temp) > longest {
+			longest = len(temp)
+			number = i
+		}
+		temp = nil
+	}
+
+	fmt.Println("-----------------------------------------------")
+	fmt.Println("Lower bound:", l, "Upper bound:", h)
+	fmt.Println("Longest:", longest, "Number:", number)
+	fmt.Println("-----------------------------------------------")
 }
