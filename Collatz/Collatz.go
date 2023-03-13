0ce5ad2f58639e0b8cafd6d23f1368463aaedcc0
Karol-Wisniewski <karol-wisniewski2001@wp.pl>
2023-03-13 18:30:51 +0100

    Finished Collatz sequence exercises

--- Collatz/Collatz.go
+++ Collatz/Collatz.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"os"
+	"strconv"
+	"strings"
 )
 
 func even(n int) (b bool) {
@@ -81,23 +81,78 @@ func generateCollatzSequences(n int) map[int]int {
 	return result
 }
 
+func generateCollatzLengths(n int) []int {
+	temp := []int{}
+	result := []int{}
+
+	for i := 1; i < n+1; i++ {
+		temp = collatz(i, temp)
+		result = append(result, len(temp))
+		temp = nil
+	}
+
+	return result
+}
+
+func flattenArray(arr [][]int) []int {
+
+	// Initialize the flattened array
+	flatArr := []int{}
+
+	// Iterate through the rows of the array
+	for _, row := range arr {
+
+		// Append the current row to the flattened array
+		flatArr = append(flatArr, row...)
+	}
+	return flatArr
+}
+
 func main() {
 	// getLongestColltazSequence(1000, 2000)
-	sequencesUnder1k := generateCollatzSequences(100000)
+	// sequencesUnder1k := generateCollatzSequences(100000)
 
-	file, err := os.Create("Collatz.dat")
+	// file, err := os.Create("Collatz.dat")
 
-	if err != nil {
-		log.Fatal(err)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// for i := 1; i < len(sequencesUnder1k)+1; i++ {
+	// 	file.WriteString(fmt.Sprintf("%d %d", i, sequencesUnder1k[i]))
+	// 	file.WriteString("\n")
+	// }
+
+	// file.Close()
+
+	lengths := generateCollatzLengths(10000)
+
+	// fmt.Println(lengths)
+
+	//remake "lengths" slice to hold the number of times each digit in each length occurs
+
+	tempArray := []int{}
+
+	for i := 0; i < len(lengths); i++ {
+
+		tempStrArray := strings.Split(strconv.Itoa(lengths[i]), "")
+
+		for i := 0; i < len(tempStrArray); i++ {
+			temp, _ := strconv.Atoi(tempStrArray[i])
+			tempArray = append(tempArray, temp)
+		}
 	}
 
-	for i := 1; i < len(sequencesUnder1k)+1; i++ {
-		file.WriteString(fmt.Sprintf("%d %d", i, sequencesUnder1k[i]))
-		file.WriteString("\n")
+	lengthsMap := make(map[int]int)
+
+	for i := 0; i < len(tempArray); i++ {
+		lengthsMap[tempArray[i]]++
 	}
 
-	file.Close()
+	fmt.Println(lengthsMap)
 
 }
 
 //Number with the longest Collatz sequence in the range 1000-2000, 2000-3000, etc. is always odd
+
+//Probability of a number occuring in a Collatz sequence is simillar to Benford's Law
